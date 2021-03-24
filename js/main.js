c65e56d5e7c767c8d22598e122517006e7255f9a
johnnyblack12 <jfebbo5@gmail.com>
2021-03-24 18:32:35 -0400

    Change input from prompt to form

--- js/main.js
+++ js/main.js
@@ -1,26 +1,33 @@
-const squares = Number(prompt('Welcome to Etch-a-Sketch. How many boxes on each side of the grid? (max 100)'));
+function go() { 
+    const form = document.getElementById("squares");
 
-const square = Math.floor(100/squares);
+    const squares = Number(form.value);
 
-const sqHeight = String(square) + 'vh'
+    const square = 100/squares
 
-const area = squares ** 2;
+    const sqHeight = String(square) + 'vh'
 
-const container = document.querySelector('.container');
+    const area = squares ** 2;
 
-let box;
-let selection;
+    const container = document.querySelector('.container');
 
-for (i=1;i<=area;i++) {
-    box = document.createElement('div');
-    box.setAttribute('id', i);
-    box.addEventListener('mouseover', (e) => {
-        selection = document.getElementById(e.fromElement.id);
-        selection.classList.add('boxHover');
-    });
-    box.style.height = sqHeight;
-    box.style.width = sqHeight;
-    container.appendChild(box);
+    let box;
+    let selection;
+    for (i=1;i<=area;i++) {
+        box = document.createElement('div');
+        box.setAttribute('id', i);
+        box.setAttribute('name', 'box');
+        box.addEventListener('mouseover', (e) => {
+            selection = document.getElementById(e.fromElement.id);
+            selection.classList.add('boxHover');
+        });
+        box.style.height = sqHeight;
+        box.style.width = sqHeight;
+        container.appendChild(box);
+    }
 }
+
+const button = document.querySelector(".button");
+button.addEventListener('click', go);
 
 
